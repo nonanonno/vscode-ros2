787559b37ec8a5ef94b9a58ca0d4c5dc4b52dbb2
nonanonno <as.nonanonno@gmail.com>
2021-10-07 20:25:54 +0900

    test: add empty test for the extension (#12)

---
+++ client/src/test/index.ts
@@ -0,0 +1,35 @@
+import * as path from 'path';
+import * as Mocha from 'mocha';
+import * as glob from 'glob';
+
+export function run(): Promise<void> {
+  const mocha = new Mocha({
+    ui: 'tdd',
+    color: true,
+  });
+  mocha.timeout(10000);
+
+  const testRoot = __dirname;
+
+  return new Promise((resolve, reject) => {
+    glob('**/*.test.js', { cwd: testRoot }, (err, files) => {
+      if (err) {
+        return reject(err);
+      }
+      files.forEach(f => mocha.addFile(path.resolve(testRoot, f)));
+
+      try {
+        mocha.run(failures => {
+          if (failures > 0) {
+            reject(new Error(`${failures} tests failed.`));
+          } else {
+            resolve();
+          }
+        });
+      } catch (err) {
+        console.error(err);
+        reject(err);
+      }
+    });
+  });
+}
